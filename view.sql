234af101c7fc957e1fa3ca0c3f5a63212090277a
Samira de Sousa Santos <angelpetrova69@icloud.com>
2024-09-09 11:32:42 -0300

    up

---
+++ view.sql
@@ -0,0 +1,52 @@
+CREATE VIEW FuncionarioProposta AS -- Criando VIEW 
+SELECT
+    f.funcionarioId,
+    f.nomeFuncionario,
+    f.loginFuncionario,
+    f.cpfFuncionario,
+    f.telefoneFuncionario,
+    p.propostaId,
+    p.categoria,
+    p.subcategoria,
+    p.valor,
+    p.dataInicio,
+    p.dataConclusao
+FROM funcionario f
+LEFT JOIN proposta p ON f.funcionarioId = p.funcionarioId;
+
+SELECT * FROM FuncionarioProposta; -- Consultando VIEW 
+ 
+CREATE VIEW RevisaoProposta AS -- Criando VIEW 
+SELECT
+    r.revisaoId,
+    r.versao,
+    p.propostaId,
+    p.categoria, 
+    p.subcategoria,
+    p.valor,
+    p.dataInicio,
+    p.dataConclusao
+FROM revisao r
+JOIN proposta p ON r.propostaId = p.propostaId;
+
+SELECT * FROM RevisaoProposta; -- Consultando VIEW 
+
+CREATE VIEW ClienteComPropostas AS -- Criando VIEW 
+SELECT
+    c.clienteId,
+    c.nomeCliente,
+    c.cpfCliente,
+    c.bairro,
+    c.cidade,
+    c.telefone1Cliente,
+    p.propostaId,
+    p.valor,
+    p.dataInicio,
+    p.dataConclusao
+FROM cliente c
+LEFT JOIN proposta p ON c.clienteId = p.clienteId;
+
+SELECT * FROM ClienteComPropostas; -- Consultando VIEW
+
+CREATE VIEW funcionarioComCliente AS 
+SELECT 

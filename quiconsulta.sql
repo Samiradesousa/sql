b0ec82dbebc51ddd650dcb0680121ed5c1ff1499
Samira de Sousa Santos <angelpetrova69@icloud.com>
2024-09-09 10:39:07 -0300

    up

--- quiconsulta.sql
+++ quiconsulta.sql
@@ -1,8 +1,5 @@
-SELECT * FROM cliente WHERE cliente.clienteId IN (
-    SELECT DISTINCT p.funcionarioId
-    FROM proposta p
-    INNER JOIN funcionarioBons f ON p.funcionarioId = f.funcionarioId
-);
+SELECT * FROM cliente WHERE cliente.clienteId IN ( SELECT DISTINCT p.funcionarioId
+FROM proposta p INNER JOIN funcionarioBons f ON p.funcionarioId = f.funcionarioId);
 
 SELECT f.nomeFuncionario, p.categoria, p.valor FROM funcionario f
 LEFT JOIN proposta p ON p.funcionarioId = f.funcionarioId;
@@ -10,7 +7,7 @@ LEFT JOIN proposta p ON p.funcionarioId = f.funcionarioId;
 SELECT f.nomeFuncionario, c.nomeCliente FROM funcionario f 
 LEFT JOIN cliente c ON c.clienteId = f.funcionarioId;
 
-SELECT f.nomeFuncionario, c.nomeCliente, p.dataInicio, p.dataConclusao FROM funcionario f
+SELECT f.nomeFuncionario, p.propostaId, c.nomeCliente, p.dataInicio, p.dataConclusao  FROM funcionario f
 LEFT JOIN cliente c, proposta p ON c.clienteId = f.funcionarioId = p.propostaId;
 
 SELECT DISTINCT f.nomeFuncionario, p.categoria, p.valor FROM funcionario f
